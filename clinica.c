7d6cda310743ad9eefe0e7e66417b000bc46ce0e
isazvdd <isadora.azevedo.700@ufrn.edu.br>
2021-10-21 11:20:28 -0300

    clinica

--- clinica.c
+++ clinica.c
@@ -17,13 +17,13 @@ void informacoesProjeto()
 {
   printf("\n");
   printf("###############################################################\n");
-  printf("        Universidade Federal do Rio Grande do Norte   \n");
-  printf("        CERES - Centro de Ensino Superior do Sérido    \n");
-  printf("        DCT - Departamento de Computação e Tecnologia  \n");
-  printf("        Disciplina - Programação -- DCT1106             \n");
-  printf("   Projeto Sistema de Agendamento para Clínicas Médicas  \n");
-  printf("    Desenvolvido por: @isazvdd e  @Joyce8900 - Out, 2021 \n");
-  printf("##########################################################\n");
+  printf("         Universidade Federal do Rio Grande do Norte           \n");
+  printf("         CERES - Centro de Ensino Superior do Sérido           \n");
+  printf("        DCT - Departamento de Computação e Tecnologia          \n");
+  printf("            Disciplina - Programação -- DCT1106                \n");
+  printf("    Projeto Sistema de Agendamento para Clínicas Médicas       \n");
+  printf("    Desenvolvido por: @isazvdd e  @Joyce8900 - Out, 2021       \n");
+  printf("###############################################################\n");
 }
 
 void informacoesEquipe()
