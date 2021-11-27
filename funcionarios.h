ba0c8c6d051a5a763848c7331e9afe9d23dec605
Joyce <joyce.santos.709@ufrn.edu.br>
2021-11-27 15:53:15 -0300

    funcionarios.h

---
+++ funcionarios.h
@@ -0,0 +1,9 @@
+// Assinaturas da função
+
+char menuPrincipal(void);
+void menuFuncionarios(void);
+void menuCurriculo(void);
+void menuCFuncionarios(void);
+void menuAtualizarF(void);
+void menuPesquisarFuncionario(void);
+void equipe(void);
