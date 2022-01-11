1a4833c1f1a4bb39a29251566f3eb0bdcb5b5505
JOYCE SANTOS <92751739+Joyce8900@users.noreply.github.com>
2022-01-11 00:50:40 +0000

    Typedet struct

--- paciente.h
+++ paciente.h
@@ -1,4 +1,18 @@
 // Assinatura das funções
+typedef struct paciente Paciente;
+struct paciente{
+char nome [60];
+char data [11];
+char celular [12];
+char cpf [12];
+char email [60];
+char doenca [999];
+char trat [999];
+char exame [999];
+
+}
+
+
 
 char menuPrincipal(void);
 char menuCadastro(void);
