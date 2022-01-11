407341b82dd25a6c154d4b63124923e89d213a80
isazvdd <isadora.azevedo.700@ufrn.edu.br>
2022-01-11 00:13:01 -0300

    Projeto 8/15

--- paciente.h
+++ paciente.h
@@ -1,20 +1,19 @@
 // Assinatura das funções
 typedef struct paciente Paciente;
-struct paciente{
-char nome [60];
-char data [11];
-char celular [12];
-char cpf [12];
-char email [60];
-char doenca [999];
-char trat [999];
-char exame [999];
+struct paciente {
+  char nome[60];
+  char data[11];
+  char celular[12];
+  char cpf[12];
+  char email[60];
+  char doenca[999];
+  char trat[999];
+  char exame[999];
 
 }
 
-
-
-char menuPrincipal(void);
+char
+menuPrincipal(void);
 char menuCadastro(void);
 void menuPaciente(void);
 void menuCadastroPaciente(void);
